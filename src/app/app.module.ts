9d48eb2f2a4028b697b174d7dfc69564a7582549
mamunurivinay <vinay.m@mtwlabs.com>
2019-02-20 19:07:07 +0530

    users-list

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { RouterModule , Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterTemplateComponent } from './register-template/register-template.component';
+import { RegisterReactiveComponent } from './register-reactive/register-reactive.component';
+import { AppService } from './app.service';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
+const router : Routes = [
+  // { path:'', component: AppComponent},
+  { path : '' , redirectTo:'/registation/template', pathMatch:'full' },
+  { path : 'login' , component : LoginComponent},
+  { path : 'registation/template', component: RegisterTemplateComponent},
+  { path : 'registation/reactive', component: RegisterReactiveComponent},
+  { path: '',loadChildren:"./users/users.module#UsersModule"}
+ 
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterTemplateComponent,
+    RegisterReactiveComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(router),
+    HttpClientModule
+    
+  ],
+  providers: [AppService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppService } from '../app.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  public isValid:boolean = false;
+
+  @ViewChild('email') email: any;
+  @ViewChild('password') password: any;
+
+  public loginData:any={
+    email:'',
+    password:''
+  }
+  constructor(
+    private router : Router,
+    public appService : AppService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onLogin(){
+    if(this.email.valid && this.password.valid)
+    this.appService.loginDetails(this.loginData)
+    .subscribe(
+      (response)=>{
+        console.log("gggg",response);
+        this.router.navigate(['/users'])
+        
+      }
+    )
+
+  }
+}
